3296ad689d4c41ce0e52a5772c582fff022075d4
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-06-15 14:40:11 +0500

    feat: add layout

---
+++ hotel.ui/src/layouts/Container/Container.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+import './Container.scss';
+
+function Container({children}:{children: ReactNode}){
+    return(
+        <div className="container">
+            {children}
+        </div>
+    );
+}
+
+export default Container;
